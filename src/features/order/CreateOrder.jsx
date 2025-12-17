fe806f24347f2e50842ece867e55bc6ad5a25560
975569220@qq.com <975569220@qq.com>
2025-12-17 23:29:04 +0800

    add sth

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -138,13 +138,13 @@ function CreateOrder() {
               ? "Placing order..."
               : `Order now from ${formatCurrency(totalPrice)}`}
           </Button>
-          <button
+          {/* <button
             type="submit"
             disabled={isSubmitting}
             className="inline-block rounded-full bg-yellow-400 px-4 py-3 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed"
           >
             {isSubmitting ? "Placing order..." : "Order Now"}
-          </button>
+          </button> */}
         </div>
       </Form>
     </div>
